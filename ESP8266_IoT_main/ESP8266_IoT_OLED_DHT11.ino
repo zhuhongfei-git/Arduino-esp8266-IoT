acc13864351fde04982296524fecc8c13aa8227a
zhuhongfei <zhuhongfei@telchina.net>
2019-10-14 21:52:55 +0800

    [NEW]创建新工程，已完成三部分内容：OLED、DHT11和WiFi-AP

---
+++ ESP8266_IoT_main/ESP8266_IoT_OLED_DHT11.ino
@@ -0,0 +1,146 @@
+/******************************************************************************
+*
+*  Copyright (C), 2016-2036, REMO Tech. Co., Ltd.
+*
+*******************************************************************************
+*  File Name     : ESP8266_IoT_OLED.ino
+*  Version       : Initial Draft
+*  Author        : zhuhongfei
+*  Created       : 2019/10/10
+*  Last Modified :
+*  Description   : OLED(128*64,0.9),base on Arduino lib
+*  Function List :
+*
+*
+*  History:
+* 
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created file
+*
+******************************************************************************/
+
+/*==============================================*
+ *      include header files                    *
+ *----------------------------------------------*/
+
+#include <Wire.h>
+#include <ACROBOTIC_SSD1306.h>
+
+//#include <DHT.h>
+/*==============================================*
+ *      constants or macros define              *
+ *----------------------------------------------*/
+//DHT11
+//#define DHTPIN    5
+//#define DHTTYPE   DHT11
+
+
+
+/*==============================================*
+ *      project-wide global variables           *
+ *----------------------------------------------*/
+//DHT11
+float DHT11_temperature = 0;
+float DHT11_humidity	= 0;
+
+//extern DHT dht;
+
+
+
+/*==============================================*
+ *      routines' or functions' implementations *
+ *----------------------------------------------*/
+/*****************************************************************************
+*   Prototype    : OLED_display_init
+*   Description  : initiation OLED 
+*   Input        : void
+*   Output       : None
+*   Return Value : void
+*   Calls        : 
+*   Called By    : 
+*
+*   History:
+* 
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created function
+*
+*****************************************************************************/
+void OLED_display_init( void )
+{
+    Wire.begin();
+    oled.init();                      // Initialze SSD1306 OLED display
+    oled.clearDisplay();              // Clear screen
+
+
+    oled.setTextXY(0, 0);             // Set cursor position, start of line 0
+    oled.putString("YUERONG");
+    oled.setTextXY(1, 0);             // Set cursor position, start of line 1
+    oled.putString("I Love You");
+    oled.setTextXY(2, 0);             // Set cursor position, start of line 2
+    oled.putString("Forever");
+    oled.setTextXY(3, 0);
+    oled.putString("Temp:     *C");
+    oled.setTextXY(4, 0);
+    oled.putString("Humi:     %R");
+}
+
+/*****************************************************************************
+*   Prototype    : OLED_run_function
+*   Description  : display content
+*   Input        : void
+*   Output       : None
+*   Return Value : void
+*   Calls        : 
+*   Called By    : 
+*
+*   History:
+* 
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created function
+*
+*****************************************************************************/
+void OLED_run_function(  void)
+{
+    oled.setTextXY(3, 5);
+    oled.putFloat(DHT11_temperature);
+
+    oled.setTextXY(4, 5);
+    oled.putFloat(DHT11_humidity);
+
+}
+
+/*****************************************************************************
+*   Prototype    : DHT11_read_sensor
+*   Description  : sample data
+*   Input        : void
+*   Output       : None
+*   Return Value : void
+*   Calls        : 
+*   Called By    : 
+*
+*   History:
+* 
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created function
+*
+*****************************************************************************/
+void DHT11_read_sensor( void )
+{
+    delay(2000);
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    DHT11_humidity = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    DHT11_temperature = dht.readTemperature();
+
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(DHT11_humidity) || isnan(DHT11_temperature))
+    { 
+        return;
+    }
+}
---
+++ ESP8266_IoT_main/ESP8266_IoT_WiFi.ino
@@ -0,0 +1,117 @@
+/******************************************************************************
+*
+*  Copyright (C), 2016-2036, REMO Tech. Co., Ltd.
+*
+*******************************************************************************
+*  File Name     : ESP8266_IoT_WiFi.ino
+*  Version       : Initial Draft
+*  Author        : zhuhongfei
+*  Created       : 2019/10/10
+*  Last Modified :
+*  Description   : wifi config
+*  Function List :
+*
+*
+*  History:
+*
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created file
+*
+******************************************************************************/
+
+/*==============================================*
+ *      include header files                    *
+ *----------------------------------------------*/
+#include "ESP8266WiFi.h"
+
+
+
+/*==============================================*
+ *      constants or macros define              *
+ *----------------------------------------------*/
+#define WIFI_CHANNAL      3
+#define WIFI_HIDDEN       0
+#define WIFI_MAX_CONNECT  4
+
+
+/*==============================================*
+ *      project-wide global variables           *
+ *----------------------------------------------*/
+const char *def_ap_password = "12345678";
+const char *def_ap_ssid = "Telchinaxf";
+
+
+/*==============================================*
+ *      routines' or functions' implementations *
+ *----------------------------------------------*/
+/*****************************************************************************
+*   Prototype    : setup_WiFi_mode
+*   Description  : WiFi config
+*   Input        : None
+*   Output       : None
+*   Return Value : void
+*   Calls        : 
+*   Called By    : 
+*
+*   History:
+* 
+*       1.  Date         : 2019/10/10
+*           Author       : zhuhongfei
+*           Modification : Created function
+*
+*****************************************************************************/
+void setup_WiFi_mode()
+{
+#if DEBUG_KEY
+    DEBUG_UART_PRINTLN("Setting soft-AP ... ");
+#endif
+    WiFi.mode(WIFI_AP);//WIFI_AP_STA
+    IPAddress softLocal(192, 168, 4, 1);
+    IPAddress softGateway(192, 168, 4, 1);
+    IPAddress softSubnet(255, 255, 255, 0);
+
+    WiFi.softAPConfig(softLocal, softGateway, softSubnet);
+    String ap_name=("ESP"+(String)ESP.getChipId());
+    const char *ap_ssid	  = ap_name.c_str();
+     WiFi.softAP(ap_ssid, def_ap_password, WIFI_CHANNAL, WIFI_HIDDEN,
+                    WIFI_MAX_CONNECT);
+/*
+    load_ap_params();
+    if ((*ap_ssid != NULL) && (*ap_pwd != NULL))
+    {
+        WiFi.softAP(ap_ssid, ap_pwd, WIFI_CHANNAL, WIFI_HIDDEN, WIFI_MAX_CONNECT);
+    }
+
+    else
+    {
+        WiFi.softAP(def_ap_ssid, def_ap_password, WIFI_CHANNAL, WIFI_HIDDEN,
+                    WIFI_MAX_CONNECT);
+    }
+
+*/
+    IPAddress myIP = WiFi.softAPIP();
+#if DEBUG_KEY
+    DEBUG_UART_PRINT("AP IP address: ");
+    DEBUG_UART_PRINTLN(myIP);
+#endif
+
+
+    //station mode ,not used now
+    //TODO:1.will be used;2.if->while delay(500ms)+try connect count
+
+    //DEBUG_UART_PRINTLN("Starting soft-sta ... ");
+    //WiFi.begin(ap_ssid, ap_password);//ssid, password
+    /*if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+    	DEBUG_UART_PRINTF("WiFi Failed!\n");
+    	return;
+    }
+
+    DEBUG_UART_PRINT("IP Address: ");
+    DEBUG_UART_PRINTLN(WiFi.localIP());
+    DEBUG_UART_PRINT("Hostname: ");
+    DEBUG_UART_PRINTLN(WiFi.hostname());*/
+
+}
+
